60796026455ab7cc53c6b9e366b23b05e4caf32a
Damian J-J <soc.acc44@gmail.com>
2020-07-08 21:08:50 +0200

    project created. Nuggets installed(mvvm light + material design)

---
+++ SimpleCurrencyExchange/SimpleCurrencyExchange/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using CommonServiceLocator;
+
+namespace SimpleCurrencyExchange.ViewModel
+{
+    
+    public class ViewModelLocator
+    {
+        
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);        
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+        
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
